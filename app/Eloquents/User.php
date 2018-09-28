f17311f3a5e45837130713874b987bfe6f28fc72
sizukutamago <tanaka@tam-bourine.co.jp>
2018-09-28 17:41:05 +0900

    rename app\user to app\eloquents\user

--- app/Eloquents/User.php
+++ app/Eloquents/User.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Eloquents;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
